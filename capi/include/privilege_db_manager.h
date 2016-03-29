2e8b36b4f2d9e130603438867a5b873d45d9c7fe
Yunjin Lee <yunjin-.lee@samsung.com>
2016-03-29 14:22:06 +0900

    Apply Tizen coding rule to headers
    Change-Id: I917f67691ba8d177e2dd7f89747ddff78c6c5940
    Signed-off-by: Yunjin Lee <yunjin-.lee@samsung.com>

--- capi/include/privilege_db_manager.h
+++ capi/include/privilege_db_manager.h
@@ -1,7 +1,7 @@
 /*
- * Copyright (c) 2013-2015 Samsung Electronics Co., Ltd All Rights Reserved
+ * Copyright(c)2013-2015 Samsung Electronics Co., Ltd All Rights Reserved
  *
- * Licensed under the Apache License, Version 2.0 (the License);
+ * Licensed under the Apache License, Version 2.0(the License);
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -19,46 +19,42 @@
 
 #include <glib.h>
 
-typedef struct
-{
-    int profile_id;
-    char* profile;
-    int package_type_id;
-    char* package_type;
-    char* privilege_name;
-    char* privilege_display;
-    char* privilege_description;
-    int privilege_level_id;
-    char* privilege_level;
-    char* issued_version;
-    char* expired_version;
-    char* changed_to;
+typedef struct {
+	int profile_id;
+	char* profile;
+	int package_type_id;
+	char* package_type;
+	char* privilege_name;
+	char* privilege_display;
+	char* privilege_description;
+	int privilege_level_id;
+	char* privilege_level;
+	char* issued_version;
+	char* expired_version;
+	char* changed_to;
 } privilege_info_db_row_s;
 
-typedef enum
-{
-    PRIVILEGE_DB_MANAGER_ERR_NONE                  = 0,
-    PRIVILEGE_DB_NO_EXIST_RESULT                   = 1,
-    PRIVILEGE_DB_MANAGER_ERR_CONNECTION_FAIL       = 2,
-    PRIVILEGE_DB_MANAGER_ERR_INVALID_QUERY         = 3,
-    PRIVILEGE_DB_MANAGER_ERR_OUT_OF_MEMORY         = 4,
+typedef enum {
+	PRIVILEGE_DB_MANAGER_ERR_NONE				  = 0,
+	PRIVILEGE_DB_NO_EXIST_RESULT				   = 1,
+	PRIVILEGE_DB_MANAGER_ERR_CONNECTION_FAIL	   = 2,
+	PRIVILEGE_DB_MANAGER_ERR_INVALID_QUERY		 = 3,
+	PRIVILEGE_DB_MANAGER_ERR_OUT_OF_MEMORY		 = 4,
 } privilege_db_manager_error_e;
 
-typedef enum
-{
-    PRIVILEGE_DB_MANAGER_PRIVILEGE_LEVEL_PUBLIC    =   0,
-    PRIVILEGE_DB_MANAGER_PRIVILEGE_LEVEL_PARTNER   =   1,
-    PRIVILEGE_DB_MANAGER_PRIVILEGE_LEVEL_PLATFORM  =   2
+typedef enum {
+	PRIVILEGE_DB_MANAGER_PRIVILEGE_LEVEL_PUBLIC	=   0,
+	PRIVILEGE_DB_MANAGER_PRIVILEGE_LEVEL_PARTNER   =   1,
+	PRIVILEGE_DB_MANAGER_PRIVILEGE_LEVEL_PLATFORM  =   2
 } privilege_db_manager_privilege_level_e;
 
-typedef enum
-{
-    PRIVILEGE_DB_MANAGER_PACKAGE_TYPE_WRT          =   0,
-    PRIVILEGE_DB_MANAGER_PACKAGE_TYPE_CORE         =   1
+typedef enum {
+	PRIVILEGE_DB_MANAGER_PACKAGE_TYPE_WRT		  =   0,
+	PRIVILEGE_DB_MANAGER_PACKAGE_TYPE_CORE		 =   1
 } privilege_db_manager_package_type_e;
 
 #ifdef __cplusplus
-    extern "C" {
+	extern "C" {
 #endif
 
 int privilege_db_manager_get_privilege_list(const char* api_version, privilege_db_manager_package_type_e package_type, GList** privilege_list);
@@ -78,7 +74,7 @@ int __privilege_db_manager_get_privacy_list(GList** privacy_list);
 int __privilege_db_manager_get_privilege_list_by_privacy(const char* privacy, GList **privilege_list);
 
 #ifdef __cplusplus
-    }
+	}
 #endif
 
-#endif // __PRIVILEGE_DB_MANAGER_H
+#endif /* __PRIVILEGE_DB_MANAGER_H */
